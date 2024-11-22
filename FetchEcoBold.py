14332cc1747c30ec4cd4ec24a73fa28a64797c55
lota09 <tjrals120@gmail.com>
2024-11-22 17:46:05 +0900

    fixed eco bold and normal share their buffer file

--- FetchEcoBold.py
+++ FetchEcoBold.py
@@ -15,7 +15,7 @@ url='https://eco.ssu.ac.kr/bbs/board.php?bo_table=notice&page=1'
 dept="경제학과"
 level="주요 공지사항"
 
-if (title:=UpdateFetch('models/eco-title.json',url,'eco-update.txt')):
+if (title:=UpdateFetch('models/eco-title.json',url,'eco-update-bold.txt')):
     date=FetchSimilar("models/eco-date.json",url)[0]
     url=FetchSimilar("models/eco-url.json",url)[0]
 
