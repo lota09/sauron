24209320ce71d317cd8f721e7e35cd7b68c1f9cc
lota09 <tjrals120@gmail.com>
2024-11-22 14:29:00 +0900

    Fixed IndexError problem upon There's no minor announcement on the page due to major ones

--- FetchDisu.py
+++ FetchDisu.py
@@ -1,21 +1,35 @@
 '''*******************************************************************
   - Project          : The Eye Of Sauron
-  - File name        : FetchDisu.py
-  - Description      : Fetch Disu Anouncement
+  - File name        : FetchDisuBold.py
+  - Description      : Fetch Disu Major Announcement
   - Owner            : Seokmin.Kang
   - Revision history : 1) 2024.11.21 : Initial release
 *******************************************************************'''
 
 from autoscraper import AutoScraper
-from Update import UpdateFetch,FetchSimilar
+from Update import UpdateFetch, FetchSimilar
 import Notify
+from Errors import *
 
 scraper = AutoScraper()
-url='https://www.disu.ac.kr/community/notice?cidx=42&page=1'
-profile="차세대반도체학과"
+url = 'https://www.disu.ac.kr/community/notice?cidx=42&page='
+page = 1
+profile = "차세대반도체학과"
 
-if (title:=UpdateFetch('models/disu-title.json',url,'disu-update.txt')):
-    date=FetchSimilar("models/disu-date.json",url)[0]
-    url=FetchSimilar("models/disu-url.json",url)[0]
+# 최대 5번까지 시도
+while page < 5:
+    try:
+        title = UpdateFetch('models/disu-title.json', f"{url}{page}", 'disu-update.txt')
+        break
+    except IndexError:
+        if (page:= page+1) >= 5:
+            raise FetchError("Fetch Failed After 5 Pages.")
+    except:
+        raise FetchError()
+        
+# 새로운 공지일때
+if title:
+    date = FetchSimilar("models/disu-date.json", f"{url}{page}")[0]
+    url = FetchSimilar("models/disu-url.json", f"{url}{page}")[0]
 
-    Notify.Email(profile,title,date,url)
+    Notify.Email(profile, title, date, url)
